d3bb0b5bab836d59119017b47a4d7b4b7423c81b
Prince Rupapara <rupaparprince8@.com>
2025-12-10 16:12:53 +0530

    file delete

--- dist/types.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Rooms {
-    id: number;
-    name: string;
-    capacity: number;
-    cleaningStartTime: string;
-    cleaningEndTime: string;
-}
-//# sourceMappingURL=types.d.ts.map
